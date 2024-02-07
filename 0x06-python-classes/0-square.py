5d7d1b002a5ff6f3873c4a0703d34e653fdd41a0
Josephine Mwende <josymwesh45@gmail.com>
2024-02-06 23:26:10 -0800

    Python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    """python3 -c 'print(__import__("0-square").MyClass.__doc__)'"""
     pass
