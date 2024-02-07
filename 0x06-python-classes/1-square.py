c690305c04531204b4d2bec0ab96fe7f4573b88f
Josephine Mwende <josymwesh45@gmail.com>
2024-02-07 10:11:30 -0800

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""Define a class called Square"""
+
+
+class Square:
+    """Instantiate the class
+       args: size
+    """
+    def __init__(self, size):
+        self._size = size
