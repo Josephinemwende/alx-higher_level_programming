d3cc9fd52bb240981db10feae3513e0a5217fab4
Josephine mwende <josymwesh45@gmail.com>
2024-03-28 16:23:11 -0700

    permissions for the user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- script that creates the MySql server user --
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVELEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
