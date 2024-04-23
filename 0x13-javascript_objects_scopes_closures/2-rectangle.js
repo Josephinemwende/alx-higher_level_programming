c11037174539e59c1bd12d7fb8ac94c4d337b8aa
Josephine mwende <josymwesh45@gamil.com>
2024-04-23 14:43:29 -0700

    JavaScript classes

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if( w <= 0 || h <= 0) {
-      this.object = {};
+    if (w <= 0 || h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
     }else {
-      this.width = w;
-      this.height = h;
+       this.width = w;
+       this.height = h;
   }
 }
 }
