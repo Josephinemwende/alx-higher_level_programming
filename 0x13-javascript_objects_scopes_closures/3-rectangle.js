b854065d491ce284fe9c8b4e03dac50979d4ef74
Josephine mwende <josymwesh45@gamil.com>
2024-04-23 16:24:17 -0700

    JavaScript classes

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,11 +6,11 @@ class Rectangle {
       this.height = h;
     }
   }
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
-      let x = '';
+      let y = '';
       for (let j = 0; j < this.width; j++) {
-        x += 'X';
+        y += 'X';
       }
       console.log(x);
     }
