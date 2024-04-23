7e74198954723efa46556eff8bbb4a6d257565ad
Josephine mwende <josymwesh45@gamil.com>
2024-04-23 06:57:08 -0700

    JavaScript

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const [arg] = process.argv.slice(2);
+const y = 'C is fun';
+if (isNaN(arg)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < arg; i++) {
+   console.log(y);
+  }
+}
