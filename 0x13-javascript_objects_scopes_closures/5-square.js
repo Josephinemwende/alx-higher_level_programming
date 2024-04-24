e9be52e4cd7abe93eaa97fce035fa6d24b335e8c
Josephine mwende <josymwesh45@gamil.com>
2024-04-23 17:16:24 -0700

    JavaScript classes

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+const Rectangle = require('./rectangle');
+
 class Square extends Rectangle {
   constructor (size) {
-    super(size);
+    super(size, size);
   }
 }
+module.exports = Square;
