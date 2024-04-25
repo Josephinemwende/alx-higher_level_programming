143c8a4c65aebfb1df2f74ddd7283c4638363b20
Josephine mwende <josymwesh45@gamil.com>
2024-04-24 18:40:10 -0700

    JavaScript warmup

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const [arg] = process.argv.slice(2);
+function factorial (n) {
+  if( isNaN(n) || n === 0) {
+    return 1;
+  }
+    return n * factorial (n -1);
+}
+const result = factorial (parseInt(arg));
+console.log(result);
