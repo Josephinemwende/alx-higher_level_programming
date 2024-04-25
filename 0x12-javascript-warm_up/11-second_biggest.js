29e15977923c2d674b12c08c1f7a195b68eff39d
Josephine mwende <josymwesh45@gamil.com>
2024-04-24 19:41:06 -0700

    JavaScript

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+function secondBiggest (nums) {
+  if (nums.length <= 1) {
+    return 0;
+  }
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,15 @@
 #!/usr/bin/node
 const [arg] = process.argv.slice(2);
-const y = 'X' * arg;
+
 if (isNaN(arg)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < arg; i++) 
-    console.log(y);
+  const size = parseInt(arg);
+  for (let i = 0; i < size; i++) {
+    let x = '';
+    for (let j = 0; j < size; j++) {
+      x += 'X';
+    }
+    console.log(x);
   }
 }
