48b088a90d21f19656a0261d1851916c46b67965
Josephine mwende <josymwesh45@gmail.com>
2024-06-11 07:52:45 -0700

    import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""program that prints the number of and the list of its arguments"""
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv) - 1
+    if argc == 0: 
+        print("0 arguments.")
+    elif argc == 1:
+        print("I argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
+
