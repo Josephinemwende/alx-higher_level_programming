b2d8381ef3cc32f874ce55a2ab1573e6b81d96d2
Josephine mwende <josymwesh45@gmail.com>
2024-06-11 05:57:27 -0700

    import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # program that imports the function def add(a, b): from the file add_0
-from add_0 import add
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    sum = add(a, b)
-    print("{} + {} = {}".format(a, b, sum))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
