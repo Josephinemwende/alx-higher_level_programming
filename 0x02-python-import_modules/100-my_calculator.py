564f90d8f9a959b6416b091d7e8c5e7cae5c6c2d
Josephine mwende <josymwesh45@gmail.com>
2024-06-12 11:52:21 -0700

    import modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""program that imports all functions from the file calcuator_1.py
+   and handles basic operations"""
+from calculator_1 import add, sub, mul, div
+    args = sys.argv
+    if args
+
