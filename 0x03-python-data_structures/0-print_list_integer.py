c989b73d7f21e6734a2501a66f3fda422966fd53
Josephine mwende <josymwesh45@gmail.com>
2024-06-14 11:25:19 -0700

    python data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,7 @@
 def print_list_integer(my_list=[]):
     my_list = [1, 2, 3, 4, 5]
     for values in my_list:
-        print(str.format("{}", values))
+        print("{}".format(values))
 
 
 print_list_integer()
