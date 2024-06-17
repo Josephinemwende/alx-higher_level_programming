f2375fae8596d3619cbcd5db06a8aa5f29d5500f
Josephine mwende <josymwesh45@gmail.com>
2024-06-17 12:05:05 -0700

    import modules

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """A function that replaces an element in a list
 at as specific position"""
+
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
